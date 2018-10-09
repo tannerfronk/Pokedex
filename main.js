9b723cc130c83efde708aa811ed7209489af1c64
Tanner <tannerjace07@hotmail.com>
2018-10-09 11:01:24 -0600

    many many changes with bootstrap/js.

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
-fetch('https://swapi.co/api/peaople/1')
+let charName = document.querySelector("#charName")
+
+fetch('https://swapi.co/api/people/1')
   .then(function(response) {
     return response.json();
   })
   .then(function(myJson) {
+    charName.textContent = myJson.name
     console.log(JSON.stringify(myJson));
   }); 
