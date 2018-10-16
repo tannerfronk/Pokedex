806850a88e0daf4680af221d5bac66553dbb1e53
Tanner <tannerjace07@hotmail.com>
2018-10-16 11:17:04 -0600

    added movie titles

--- main.js
+++ main.js
@@ -9,4 +9,15 @@ fetch('https://swapi.co/api/people/1')
     console.log(JSON.stringify(myJson));
   }); 
 
-import { films } from './assets/main.js'
+import { films } from './assets/films.js'
+
+console.log(films)
+
+let cardList = document.querySelector("#cardList")
+
+films.forEach((film) => {
+    let listItem = document.createElement('li')
+    listItem.textContent = film.title
+    cardList.appendChild(listItem)
+    console.log(film.title)
+})
