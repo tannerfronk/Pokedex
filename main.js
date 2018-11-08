cfee8771edf493cbcdca765270def059ef7f73c1
Tanner <tannerjace07@hotmail.com>
2018-11-08 11:16:56 -0700

    started pokedex, added img and js

--- main.js
+++ main.js
@@ -21,3 +21,27 @@ films.forEach((film) => {
     cardList.appendChild(listItem)
     console.log(film.title)
 }) //end of star wars API code
+
+// Pokemon Data
+
+import { pokemon } from "./assets/pokemon.js"
+
+console.log(pokemon)
+
+const pokeContainer = document.querySelector("#pokeContent")
+let count = 1
+
+
+pokemon.forEach(element => {
+    let numPrefix = count < 10 ? '00' : '0'
+    let imgName = `${numPrefix}${count++}${element.name}.png`
+    let fig = document.createElement('figure')
+    let cap = document.createElement('figcaption')
+    let img = document.createElement('img')
+    img.src = `img/${imgName}`
+    cap.textContent = element.ename
+    fig.appendChild(img)
+    fig.appendChild(cap)
+    pokeContainer.appendChild(fig)
+})
+               
